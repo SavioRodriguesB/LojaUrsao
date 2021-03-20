108d24464a7a3fc77683a97e5201456848dbddbe
SavioRodrigues <saviobrjf28@gmail.com>
2021-03-19 22:34:11 -0300

    Adicionando classeUsuarioLogado

---
+++ LojaUrsinho/src/main/java/to/usuarioLogado.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package to;
+
+/**
+ *
+ * @author savio
+ */
+public class usuarioLogado {
+    
+    Integer id;
+    Integer idUsuario;
+    String Login;
+    String Senha;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String Login) {
+        this.Login = Login;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+    
+    
+    
+    
+}
